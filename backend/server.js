79f1e31049dab6d97c0cf4a4e7efc07ca4ced5fa
vclorenzo <vanz.lorenzo@whitecloak.com>
2024-04-20 03:37:11 +0800

    setup mongoDB on server

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
+
 const workoutRoutes = require('./routes/workouts');
 
 const app = express();
-
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -13,6 +15,13 @@ app.use((req, res, next) => {
 
 app.use('/api/workouts', workoutRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log('listening to port ', process.env.PORT);
-});
+//connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen to db
+    app.listen(process.env.PORT, () => {
+      console.log('connected to db and listening to port ', process.env.PORT);
+    });
+  })
+  .catch((err) => console.log(err));
